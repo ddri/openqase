dd2d3c1d598179043db4b1218af346f3caf9328a
ddri <davedri@gmail.com>
2025-07-31 07:26:17 +1000

    chore: admin tidy up

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/nextjs';
 export function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     Sentry.init({
-      dsn: "https://ff1825ae5045120180d513a95885cb17@o4509319180189696.ingest.us.sentry.io/4509319185367040",
+      dsn: process.env.SENTRY_DSN,
       environment: process.env.NODE_ENV,
       tracesSampleRate: 1.0,
       debug: process.env.NODE_ENV === 'development',
@@ -12,7 +12,7 @@ export function register() {
 
   if (process.env.NEXT_RUNTIME === 'edge') {
     Sentry.init({
-      dsn: "https://ff1825ae5045120180d513a95885cb17@o4509319180189696.ingest.us.sentry.io/4509319185367040",
+      dsn: process.env.SENTRY_DSN,
       environment: process.env.NODE_ENV,
       tracesSampleRate: 1.0,
       debug: process.env.NODE_ENV === 'development',

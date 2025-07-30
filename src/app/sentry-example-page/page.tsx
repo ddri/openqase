22794aa90eb708801ec02c48aaf4c5869b206e9a
ddri <davedri@gmail.com>
2025-07-31 07:34:07 +1000

    chore: admin tidy up

--- src/app/sentry-example-page/page.tsx
+++ src/app/sentry-example-page/page.tsx
@@ -40,7 +40,7 @@ export default function Page() {
         </h1>
 
         <p className="description">
-          Click the button below, and view the sample error on the Sentry <a target="_blank" href="https://openqase-9y.sentry.io/issues/?project=4509319185367040">Issues Page</a>.
+          Click the button below, and view the sample error on the Sentry <a target="_blank" href={`https://openqase-9y.sentry.io/issues/?project=${process.env.NEXT_PUBLIC_SENTRY_PROJECT_ID || '4509319185367040'}`}>Issues Page</a>.
           For more details about setting up Sentry, <a target="_blank" href="https://docs.sentry.io/platforms/javascript/guides/nextjs/">read our docs</a>.
         </p>
 
